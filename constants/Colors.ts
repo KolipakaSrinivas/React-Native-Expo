5f9f765dbfdd510bced1b117ed6cfbe1c74d8b64
Srinivas <kolipakasrinivas456@gmail.com>
2025-02-05 09:37:50 +0530

    complated CoffeeShop app

---
+++ constants/Colors.ts
@@ -0,0 +1,36 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+import { Button } from "react-native";
+
+const tintColorLight = 'orange';
+const tintColorDark = '#fff';
+
+export const Colors = {
+  light: {
+    text: '#11181C',
+    background: '#ffffff',
+    headerBackground: 'rgb(242,242,242)',
+    tint: "blue",
+    icon: '#687076',
+    tabIconDefault: '#c68e17',
+    tabIconSelected: tintColorLight,
+    blue:"blue",
+    titleColor:"black",
+    button:"smoke"
+  },
+  dark: {
+    text: '#ECEDEE',
+    background: '#151718',
+    headerBackground: 'rgb(1,1,1)',
+    tint: tintColorDark,
+    icon: '#9BA1A6',
+    tabIconDefault: '#9BA1A6',
+    tabIconSelected: tintColorDark,
+    blue:"blue",
+    titleColor:"white",
+    button:"gray"
+  },
+};
